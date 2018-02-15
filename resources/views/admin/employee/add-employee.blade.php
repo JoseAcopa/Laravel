687c571b455752da29fb40ebfe9865c9de90f3a3
Jose Acopa <jose_acopa@hotmail.com>
2018-02-15 02:13:24 -0600

    nav agregadas en todas las vistas

--- resources/views/admin/employee/add-employee.blade.php
+++ resources/views/admin/employee/add-employee.blade.php
@@ -10,31 +10,7 @@
   </head>
   <body>
     <header>
-      <nav class="nav">
-        <ul class="ul-nav">
-          <li onclick="menuVertical()"><i  class="fa fa-bars" aria-hidden="true"></i></li>
-          <li>RAYOS X Y SERVICIOS INDUSTRIALES S.A. DE C.V.</li>
-          <div class="sesion">
-            <ul>
-              <li><img src="{{ url('img/image.png')}}" alt="" class="popout">
-                <ul>
-                  <div class="photo">
-                    <img src="{{ url('img/image.png')}}" alt="">
-                  </div>
-                  <div class="name">
-                    <h3>Nirandelli Patricio Mayo</h3>
-                    <h3></h3>
-                  </div>
-                  <li></li>
-                  <div class="footerSingout">
-                    <a href="#" class="sign-out"><i class="fa fa-sign-out"></i> Cerrar Sesión</a>
-                  </div>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </ul>
-      </nav>
+      @include('../layouts/nav')
     </header>
     <main class="wrapper">
       <aside class="menu" id="aside">
@@ -95,6 +71,12 @@
               <label for="password">Contraseña:</label>
               <input type="password" name="password" id="password" class="{{ $errors->has('password') ? 'has-error' : '' }}">
               {!! $errors->first('password','<span class="data-error">:message</span>')!!}
+              <label for="user">Privilegio:</label>
+              <select name="" class="select-design">
+                <option value=""></option>
+                <option value="">Administrador</option>
+                <option value="">Usuario</option>
+              </select>
             </div>
             <div class="button-client">
               <button type="submit" class="btn-save"><i class="fa fa-save fa-lg"></i> Guardar</button>
